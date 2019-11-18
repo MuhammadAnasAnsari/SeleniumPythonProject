0660927a825e9d6295f0cd8bb7a035b87fb8372c
anasansari <anas.ansari@shahisawari.co>
2019-11-18 12:42:39 +0500

    Initial commit

---
+++ Demo/PyTest/test_parametrization_demo.py
@@ -0,0 +1,45 @@
+# To generate the html report: [in which the assets > style.css will generate with report html file]
+# 1) instal pytest html   directory>pip install pytest-html
+# In terminal / cmd give command 2) directory>pytest --html=report1.html  [name of the report with .html extension]
+# =============================================
+# To generate / get self contained Html reports:[Use to send the reports with style.css file in it]
+# In terminal / cmd give command     dirctory>pytest --html=./reports/report1.html --self-contained-html
+
+# ================================================
+#  For Allure reports:
+# 1) pip install allure-pytest
+# 2) pytest --alluredir=folder directory(relative or absolute path)   # to generate reports
+# 3) Install Java, nodejs and npm
+# 4) Run this command just one time : directory>npm install -g allure-commandline --save-dev
+# Then allure serve will work
+# 5) allure serve directory
+
+
+
+import pytest
+
+
+def sum(a, b):
+    return a + b
+
+
+# Parametrise and take values(inputs and assertion) in the Tuple
+@pytest.mark.parametrize("input1, input2, output",
+                         [
+                             (3, 5, 8),
+                             (5, 6, 11),
+                             (9, 9, 20)
+                         ]
+                         )
+def test_calc_sum_1(input1, input2, output):
+    result = sum(input1, input2)
+    assert result == output
+
+# def test_calc_sum_2():
+#     result = sum(3, 3)
+#     assert result == 6
+#
+#
+# def test_calc_sum_3():
+#     result = sum(5, 5)
+#     assert result == 10
---
+++ SampleProjects/GoogleSearchTest.py
@@ -0,0 +1,39 @@
+import unittest
+import HtmlTestRunner
+from selenium import webdriver
+
+
+class GoogleSearch(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # cls.driver=webdriver.Chrome(ChromeDriverManager().install())
+        cls.driver = webdriver.Chrome("../drivers/chromedriver.exe")
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
+        print(cls.driver.title)
+
+    def test_search_automation(self):
+        self.driver.get("https://www.google.com")
+        self.driver.find_element_by_name("q").send_keys("Automation Step by Step")
+        self.driver.find_element_by_name("btnK").click()
+        print(self.driver.title)
+
+    def test_search_myName(self):
+        self.driver.get("https://www.google.com")
+        self.driver.find_element_by_name("q").send_keys("Muhammad Anas Ansari")
+        self.driver.find_element_by_name("btnK").click()
+        self.driver.find_element_by_xpath("//a[contains(text(),'Images')]").click()
+        self.driver.implicitly_wait(20)
+        print(self.driver.title)
+        # self.driver.close()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        print("Test Completed....!")
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output="E:/repos/qatest/Selenium/Reports"))
